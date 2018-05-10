b1578d616aec22a83dec93ec94317ab17c546bc1
kaarel <kaarel.toomet@gmail.com>
2018-05-09 21:41:30 -0700

    initial code and sprites

---
+++ butterwars.py
@@ -0,0 +1,172 @@
+
+import pygame as pg
+import random as r
+pg.init()
+pic = pg.image.load("mrbutter1.png")
+pics = pg.image.load("mrbutter1stab.png")
+gstkm = pg.image.load("greenstickman.png")
+screen = pg.display.set_mode((0,0), pg.RESIZABLE)
+screenw = screen.get_width()
+screenh = screen.get_height()
+pg.display.set_caption("egg")
+do = True
+dist = 4
+mup = False
+mdown = False
+mleft = False
+mright = False
+gofast = False
+timer = pg.time.Clock()
+gstick = 0
+gsmax = 120
+points = 0
+hp = 5
+font = pg.font.SysFont("Times", 24)
+dfont = pg.font.SysFont("Times", 32)
+pfont = pg.font.SysFont("Times", 50)
+pause = False
+gameover = False
+player = pg.sprite.Group()
+gstickmen = pg.sprite.Group()
+class Player(pg.sprite.Sprite):
+    def __init__(self,x,y):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pic
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+    def update(self, mup, mdown, mleft, mright):
+        if self.rect.y <= 0:
+            up = False
+        else:
+            up = True
+        if self.rect.y >= screenh-100:
+            down = False
+        else:
+            down = True
+        if self.rect.x <= 0:
+            left = False
+        else:
+            left = True
+        if self.rect.x >= screenw-124:
+            right = False
+        else:
+            right = True
+        if mup and up:
+            self.rect.y -= dist 
+        if mdown and down:
+            self.rect.y += dist
+        if mleft and left:
+            self.rect.x -= dist
+        if mright and right:
+            self.rect.x += dist
+class Thing(pg.sprite.Sprite):
+    def __init__(self, x, y, img):
+        pg.sprite.Sprite.__init__(self)
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.x = float(x)
+        self.y = float(y)
+        self.rect.x = int(self.x)
+        self.rect.y = int(self.y)
+    def update(self):
+        self.x += 0
+
+def reset():
+    global points, hp, atick, gtick, atimer, gtimer, otick,\
+           omax, slowtick, sbtick, sbmax, imtick, gameover, speed,\
+           speedf
+    gameover = False
+    screen.fill((0, 0, 0))
+    points = 0
+    hp = 5
+    gstickmen.empty()
+    gstickmen.add(Thing(r.uniform(10,screenw-96),screenh, ghost))
+mb = Player(screenw/2,screenh-128)
+player.add(mb)
+gstickmen.add(Thing(r.uniform(10,screenw-90),screenh-96,gstkm))
+
+while do:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            do = False
+        elif event.type == pg.KEYDOWN:
+            if event.key == pg.K_LEFT:
+                mleft = True
+            elif event.key == pg.K_RIGHT:
+                mright = True
+            elif event.key == pg.K_p:
+                pause = True
+            elif event.key == pg.K_r:
+                reset()
+        elif event.type == pg.MOUSEBUTTONDOWN:
+            gofast = True
+        elif event.type == pg.MOUSEBUTTONUP:
+            gofast = False
+        elif event.type == pg.KEYUP:
+            if event.key == pg.K_UP:
+                mup = False
+            elif event.key == pg.K_DOWN:
+                mdown = False
+            elif event.key == pg.K_LEFT:
+                mleft = False
+            elif event.key == pg.K_RIGHT:
+                mright = False
+    while pause:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pause = False
+                do = False
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_p:
+                    pause = False
+        pd = "PAUSED"
+        ptext = dfont.render(pd, True, (127,127,127))
+        ptext_rect = ptext.get_rect()
+        ptext_rect.centerx = screen.get_rect().centerx
+        ptext_rect.y = 50
+        screen.blit(ptext,ptext_rect)
+        screen.blit(text,text_rect)
+        pg.display.update()
+    if hp <= 0:
+        uded = "GAME OVER"
+        dtext = dfont.render(uded, True, (255,0,0))
+        dtext_rect = dtext.get_rect()
+        dtext_rect.centerx = screen.get_rect().centerx
+        dtext_rect.y = 30
+        screen.blit(dtext,dtext_rect)
+        screen.blit(text,text_rect)
+        pg.display.update()
+        gameover = True
+    while gameover:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                gameover = False
+                do = False
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_r:
+                    reset()
+        uded = "GAME OVER"
+        dtext = dfont.render(uded, True, (255,0,0))
+        dtext_rect = dtext.get_rect()
+        dtext_rect.centerx = screen.get_rect().centerx
+        dtext_rect.y = 30
+        screen.blit(dtext,dtext_rect)
+        screen.blit(text,text_rect)
+        pg.display.update()
+    screen.fill((64,128,255))
+    status = ("Score: " + str(points) + " Health: " + str(hp))
+    text = font.render(status, True, (255,255,255))
+    text_rect = text.get_rect()
+    text_rect.centerx = screen.get_rect().centerx
+    text_rect.y = 10
+    screen.blit(text,text_rect)
+    player.update(mup,mdown, mleft, mright)
+    player.draw(screen)
+    gstickmen.update()
+    gstickmen.draw(screen)
+    pg.display.update()
+    if not gofast:
+        timer.tick(60)
+
+pg.quit()
